32f17a9aaf2fa5c09acec6168c1775e4df40ff8a
KamilTaras <kamiltaras0@gmail.com>
2023-11-13 11:48:41 +0100

    adding constant values (colors)

---
+++ lib/constatntValues.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constant {
+  static const mainRedColor = Color(0xE82C4E);
+  static const mainGreenColor = Color(0x00B47E);
+  static const mainBackgroundColor = Color(0xEAEAEA);
+}
