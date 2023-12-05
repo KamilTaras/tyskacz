cff479952ecbcb1acf87563deb20e4b48b698ca9
Nikodem Stamirski <nikoxx444@gmail.com>
2023-12-05 23:55:17 +0100

    map added
    added a simple map

--- lib/Pages/mapsPage.dart
+++ lib/Pages/mapsPage.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
-
-class MapPage extends StatelessWidget {
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: LatLng(51.509364, -0.128928),
+        initialZoom: 9.2,
+      ),
       children: [
-        Center(
-          child: Text(
-            'Map Page',
-            style: TextStyle(fontSize: 25),
-          ),
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        RichAttributionWidget(
+          attributions: [
+          ],
         ),
       ],
     );
   }
 }
+
