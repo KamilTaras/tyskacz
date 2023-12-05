201699c66bd1168f45eb0b518d896962337e481f
KamilTaras <kamiltaras0@gmail.com>
2023-12-05 16:00:03 +0100

    minor changes

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -7,11 +7,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Center(
-          child: Text(
-            'Home Page',
-            style: TextStyle(fontSize: 25),
-          ),
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(70.0),
+              child: Image(
+                image: AssetImage('assets/photos/logo_TySkacz_light.png'),
+              ),
+            ),
+            Row(
+              children: [
+                ElevatedButton(onPressed: () {}, child: Container()),
+                ElevatedButton(onPressed: () {}, child: Container())
+              ],
+            ),
+            Text(
+              'Home Page',
+              style: TextStyle(fontSize: 25),
+            ),
+          ],
         ),
       ],
     );
