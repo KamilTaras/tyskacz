f9e7963023b80b3a494cd08a9c178a535d931526
Zuzia780 <mikusia2001@gmail.com>
2024-01-05 07:28:04 +0100

    sign in page, not pretty yet

--- lib/Pages/AuthorisationPages/signIn.dart
+++ lib/Pages/AuthorisationPages/signIn.dart
@@ -1,32 +1,100 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class SignIn extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _SignInState createState() => _SignInState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Home Page'),
       ),
-      body: Center(
-        child: Container(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
 
-          width: 150,
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-          child: FilledButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(
-              'Welcome',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  'Sign In',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+              ),
             ),
-          ),
+            SizedBox(height:20),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone number, email or username',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+
+            SizedBox(
+              height: 65,
+              width: 360,
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: FilledButton(
+                    child: Text( 'Log in ', style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    onPressed: (){
+                      print('Successfully log in ');
+                    },
+
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+                child: Center(
+                  child: Row(
+                    children: [
+
+                      Padding(
+                        padding: const EdgeInsets.only(left: 62),
+                        child: Text('Forgot your login details? '),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(left:1.0),
+                        child: InkWell(
+                            onTap: (){
+                              print('hello');
+                            },
+                            child: Text('Get help logging in.', style: TextStyle(fontSize: 14, color: Colors.blue),)),
+                      )
+                    ],
+                  ),
+                )
+            )
+          ],
         ),
       ),
     );
