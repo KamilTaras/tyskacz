2d5f10504309da74edfa00a4e8504ceedbc54016
Nikodem Stamirski <nikoxx444@gmail.com>
2024-01-10 14:43:08 +0100

    Fixed calendar

--- lib/Pages/NavBarPages/calendarPage.dart
+++ lib/Pages/NavBarPages/calendarPage.dart
@@ -19,51 +19,43 @@ class _UserCalendarPageState extends State<UserCalendarPage> {
   DatabaseService databaseService = DatabaseService();
 
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Event>>(
-        future: databaseService.getEvents(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          }
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Text('No events found');
-          }
-          List<Event> eventList = snapshot.data!;
-          return CalendarPage(eventList: eventList);
-        });
+    return CalendarPage(
+        child:
+        FutureBuilder<List<Event>>(
+            future: databaseService.getEvents(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return Calendar(eventList: []);
+              }
+              List<Event> eventList = snapshot.data!;
+              return Calendar(eventList: eventList);
+            })
+    );
   }
 }
 
 class CalendarPage extends StatelessWidget {
-  CalendarPage({Key? key, required this.eventList});
+  CalendarPage({Key? key, required this.child});
 
-  List<Event> eventList;
+  Widget child;
 
   @override
   Widget build(BuildContext context) {
-    final double screenHeight = MediaQuery.of(context).size.height;
-    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+    final double screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
     final double spaceUnderTitle = screenHeight * 0.05;
 
-    print(eventList);
-    final Map<DateTime, List<Attraction>> eventsMap = {};
-
-    eventList.forEach((event) {
-      DateTime startDate = event.startDate;
-      DateTime endDate = event.endDate;
-
-      for (var date = startDate;
-          date.isBefore(endDate);
-          date = date.add(Duration(days: 1))) {
-        eventsMap.putIfAbsent(date, () => []);
-        eventsMap[date]!.add(event.attractionWithinEvent);
-      }
-    });
-
-
-
     return Stack(
-        children:[
+        children: [
           Background(),
           Scaffold(
             backgroundColor: Colors.transparent,
@@ -77,22 +69,21 @@ class CalendarPage extends StatelessWidget {
                   children: [
                     Text(
                       'Calendar Page',
-                        style:  TextStyle(fontFamily: 'MainFont', fontSize: 40, color: Colors.grey[900]),
+                      style: TextStyle(fontFamily: 'MainFont',
+                          fontSize: 40,
+                          color: Colors.grey[900]),
                     ),
                     SizedBox(height: spaceUnderTitle),
                     Container(
-                      height: MediaQuery.of(context).size.height - // Adjust the height as needed
-                          kToolbarHeight - MediaQuery.of(context).padding.top - 20, // Additional padding
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: eventsMap.length,
-                        itemBuilder: (context, index) {
-                          DateTime date = eventsMap.keys.elementAt(index);
-                          List<Attraction> attractions = eventsMap[date]!;
-                          return DayOfEventsEntry(
-                            date: date, attractions: attractions);
-                        },
-                      ),
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .height - // Adjust the height as needed
+                          kToolbarHeight - MediaQuery
+                          .of(context)
+                          .padding
+                          .top - 20, // Additional padding
+                      child: child,
                     ),
                   ],
                 ),
@@ -105,52 +96,100 @@ class CalendarPage extends StatelessWidget {
 
 }
 
+Map<DateTime, List<Attraction>> mapToDays(List<Event> eventList) {
+  final Map<DateTime, List<Attraction>> eventsMap = {};
+
+  eventList.forEach((event) {
+    DateTime startDate = event.startDate;
+    DateTime endDate = event.endDate;
+
+    for (var date = DateTime(startDate.year, startDate.month, startDate.day);
+    date.isBefore(endDate);
+    date = date.add(Duration(days: 1))) {
+      eventsMap.putIfAbsent(date, () => []);
+      eventsMap[date]!.add(event.attractionWithinEvent);
+    }
+  });
+  return eventsMap;
+}
+
+
+class Calendar extends StatelessWidget {
+  Calendar({
+    super.key,
+    required this.eventList,
+  });
+
+  final List<Event> eventList;
+  late final eventsMap = mapToDays(eventList);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: eventsMap.length,
+      itemBuilder: (context, index) {
+        DateTime date = eventsMap.keys.elementAt(index);
+        List<Attraction> attractions = eventsMap[date]!;
+        return DayOfEventsEntry(
+            date: date, attractions: attractions);
+      },
+    );
+  }
+}
+
 class DayOfEventsEntry extends StatelessWidget {
   const DayOfEventsEntry(
-      {Key? key, required this.date, required this.attractions}) : super(key: key);
+      {Key? key, required this.date, required this.attractions})
+      : super(key: key);
   final DateTime date;
   final List<Attraction> attractions;
 
   @override
   Widget build(BuildContext context) {
-
     return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Text(
-                DateFormat('dd MMMM yyyy').format(date),
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: mainGreen[200], fontFamily: 'Anton-Regular'),
-              ),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              DateFormat('dd MMMM yyyy').format(date),
+              style: TextStyle(fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: mainGreen[200],
+                  fontFamily: 'Anton-Regular'),
             ),
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              color: Colors.white,
-              child: Column(
-                children: attractions.map((attraction) {
-                  return ListTile(
-                    contentPadding: const EdgeInsets.all(8.0),
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Image.network(
-                        attraction.photoURL,
-                        width: 70,
-                        height: 70,
-                        fit: BoxFit.cover,
-                      ),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            color: Colors.white,
+            child: Column(
+              children: attractions.map((attraction) {
+                return ListTile(
+                  contentPadding: const EdgeInsets.all(8.0),
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.network(
+                      attraction.photoURL,
+                      width: 70,
+                      height: 70,
+                      fit: BoxFit.cover,
                     ),
-                    title: Text(attraction.name, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Anton-Regular')),
-                    subtitle: Text(attraction.description, style: TextStyle(fontSize: 15, fontFamily: 'Anton-Regular')),
-                  );
-                }).toList(),
-              ),
+                  ),
+                  title: Text(attraction.name, style: TextStyle(fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Anton-Regular')),
+                  subtitle: Text(attraction.description, style: TextStyle(
+                      fontSize: 15, fontFamily: 'Anton-Regular')),
+                );
+              }).toList(),
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
